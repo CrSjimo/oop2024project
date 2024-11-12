a1324366d9f78f907842d28c56499dca1f3b38ce
CrSjimo <crs_16423@outlook.com>
2024-11-12 20:59:22 +0800

    add UserData

---
+++ src/main/java/dev/sjimo/oop2024project/model/UserData.java
@@ -0,0 +1,73 @@
+package dev.sjimo.oop2024project.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserData {
+    public enum Gender {
+        OTHER,
+        MALE,
+        FEMALE,
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Enumerated(EnumType.ORDINAL)
+    private Gender gender;
+
+    private String gravatarEmail;
+
+    private String description;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getGravatarEmail() {
+        return gravatarEmail;
+    }
+
+    public void setGravatarEmail(String gravatarEmail) {
+        this.gravatarEmail = gravatarEmail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
